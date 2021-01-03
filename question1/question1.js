220e0ddd22c5c9b0b56ac242c5f7e680b1ac5d07
AsrarAlmarri <76258365+AsrarAlmarri@users.noreply.github.com>
2021-01-03 22:08:52 +0300

    done with the bonus B)

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,23 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+let me = "asrar";
+
+function sayHello(name)
+{
+    
+
+    if (name != null){
+
+        console.log(`hello ${name}`);
+
+    }
+    else {
+        console.log("hello");
+    }
+ 
+}
+sayHello();
+sayHello(me);
+
+
+
